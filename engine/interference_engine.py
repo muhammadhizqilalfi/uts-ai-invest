ed80218a33344b9859dc7c04b3b5089445ce5d31
Shaldi Shauqi <shaldishauqi2016@gmail.com>
2025-10-25 04:08:20 +0700

    Update knowledge base file path in load_knowledge_base

--- engine/interference_engine.py
+++ engine/interference_engine.py
@@ -1,7 +1,7 @@
 import json
 import re
 
-def load_knowledge_base(file_path="knowledge_base.json"):
+def load_knowledge_base(file_path="../knowledge_base/knowledge_base.json"):
     """Baca knowledge base dari file JSON"""
     with open(file_path, "r", encoding="utf-8") as f:
         data = json.load(f)
